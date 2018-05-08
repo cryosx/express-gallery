be18ecccfddc8c2168b645ddc53605bbd632eb9b
Jace Mitsuka <PJace@BigMac.local>
2018-05-08 13:47:53 -1000

    created login and register pages

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars');
 const methodOverride = require('method-override');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 
 const routes = require('./routes');
 
@@ -10,15 +13,68 @@ const app = express();
 app.engine('.hbs', handlebars({ extname: '.hbs', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
-
-
-app.use(express.static('public'));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(express.static('public'));
 app.use(methodOverride('_method'));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', routes);
+
+// after login
+passport.serializeUser((user, done) => {
+    console.log('serializing');
+    return done(null, {
+        id: user.id,
+        email: user.email
+    });
+});
+
+// after every request 
+passport.deserializeUser((user, done) => {
+    console.log('deserializing');
+    new User({ id: user.id }).fetch()
+        .then(user => {
+            user = user.toJSON();
+            return done(null, {
+                id: user.id,
+                email: user.email
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            return done(err);
+
+        });
+});
+
+passport.use(new LocalStrategy(function (email, password, done) {
+    return new User({ email: username }).fetch()
+        .then(user => {
+            user = user.toJSON();
+            console.log(user)
+            if (user === null) {
+                return done(null, false, { message: 'bad email or password' });
+            }
+            else {
+                console.log(password, user.password);
+                if (password === user.password) { return done(null, user); }
+                else {
+                    return done(null, false, { message: 'bad email or password' });
+                }
+            }
+        })
+        .catch(err => {
+            console.log('error: ', err);
+            return done(err);
+        });
+}));
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const gallery = require('./gallery.js');
 const Gallery = require('../db/models/Gallery.js');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('../db/models/User.js')
+
 const router = express.Router();
+
 
 router.route('/').get((req, res) => {
   return Gallery.fetchAll()
@@ -17,6 +23,54 @@ router.route('/').get((req, res) => {
     });
 });
 
-router.use('/gallery', gallery);
+router.route('/register')
+  .get((req, res) => {
+    return res.redirect('register.html');
+  })
+  .post((req, res) => {
+    console.log(req.body)
+    return new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    }).save()
+      .then((user) => {
+        console.log(user);
+        return res.redirect('/');
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.send('Stupid email');
+      });
+  });
+
+
+
+router.route('/login').get((req, res) => {
+
+}).post(passport.authenticate('local', {
+  successRedirect: '/secret',
+  failureRedirect: '/'
+}));
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  return res.sendStatus(200);
+});
+
+function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { next(); }
+  else { res.redirect('/'); }
+};
+
+router.get('/secret', isAuthenticated, (req, res) => {
+  console.log('req.user: ', req.user);
+  console.log('req.user id', req.user.id);
+  console.log('req.username', req.user.username);
+  res.send('you found the secret!');
+});
+
+
+router.use('/gallery', isAuthenticated, gallery);
 
 module.exports = router;
