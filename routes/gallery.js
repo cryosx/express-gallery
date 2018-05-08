af16591c9528e69c477bf4f63e6148267882a180
cryosx <cyruswu.email@gmail.com>
2018-05-07 16:33:46 -1000

    added /:id GET route

--- routes/gallery.js
+++ routes/gallery.js
@@ -2,9 +2,24 @@ const express = require('express');
 
 const router = express.Router();
 
-router.route('/')
-.get((req, res) => {
-  return res.json({ message: 'smoke test'});
-})
+router.route('/').get((req, res) => {
+  return res.json({ message: 'smoke test' });
+});
 
-module.exports = router; 
+router.route('/:id').get((req, res) => {
+  const { id } = req.params;
+  return new Gallery()
+    .where({ id })
+    .fetch()
+    .then(gallery => {
+      if (!gallery) {
+        throw new Error('Gallery photo not found');
+      }
+      return res.json(gallery);
+    })
+    .catch(err => {
+      return res.status(500).json(err);
+    });
+});
+
+module.exports = router;
