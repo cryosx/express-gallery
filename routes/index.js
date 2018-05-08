718c70320b2fa3cb5c55d8a6bca0c6b96f4531ef
cryosx <cyruswu.email@gmail.com>
2018-05-07 18:19:39 -1000

    initial / GET implementation

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const gallery = require('./gallery.js');
-
+const Gallery = require('../db/models/Gallery.js');
 const router = express.Router();
+
+router.route('/').get((req, res) => {
+  return Gallery.fetchAll()
+    .then(gallery => {
+      return res.json(gallery);
+    })
+    .catch(err => {
+      return res.json(err);
+    });
+});
 
 router.use('/gallery', gallery);
 
