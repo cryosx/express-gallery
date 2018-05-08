054fe17920149d98e478b6a1cac4fa040e1dce1d
cryosx <cyruswu.email@gmail.com>
2018-05-07 16:42:37 -1000

    fixed fixing brace/paren

--- routes/gallery.js
+++ routes/gallery.js
@@ -2,19 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 
+router
+  .route('/')
+  .get((req, res) => {
+    return res.json({ message: 'smoke test' });
+  })
+  .post((req, res) => {});
 
-router.route('/')
-.get((req, res) => {
-  return res.json({ message: 'smoke test'});
-})
-.post((req, res) => {
-  
-})
-
-router.route('/new')
-.get((req, res) => {
+router.route('/new').get((req, res) => {
   res.render('new-photo');
-
+});
 
 router.route('/:id').get((req, res) => {
   const { id } = req.params;
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,4 @@ const router = express.Router();
 
 router.use('/gallery', gallery);
 
-module.exports = router; 
+module.exports = router;
