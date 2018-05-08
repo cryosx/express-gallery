65fb7f5955f4fd6f45a0c2c980feab4ae260d210
Jace Mitsuka <PJace@BigMac.local>
2018-05-07 16:28:02 -1000

    models created

---
+++ db/models/Gallery.js
@@ -0,0 +1,9 @@
+const bookshelf = require('./bookshelf');
+
+class Gallery extends bookshelf.Model {
+  get tableName() { return 'gallery' }
+  get hasTimestamps() { return true }
+
+}
+
+module.exports = Gallery;
