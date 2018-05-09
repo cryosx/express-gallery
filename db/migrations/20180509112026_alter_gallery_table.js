fb682f90ff6209516dce72d88ce18c2feb3d9aa2
Jace Mitsuka <PJace@BigMac.local>
2018-05-09 13:41:50 -1000

    change author id to user id

--- db/migrations/20180509112026_alter_gallery_table.js
+++ db/migrations/20180509112026_alter_gallery_table.js
@@ -1,12 +1,12 @@
 
 exports.up = function (knex, Promise) {
   return knex.schema.table('gallery', table => {
-    table.integer('author_id').unsigned().notNullable().references('id').inTable('users');
+    table.integer('user_id').unsigned().notNullable().references('id').inTable('users');
   })
 };
 
 exports.down = function (knex, Promise) {
   return knex.schema.table('gallery', table => {
-    table.dropColumn('author_id');
+    table.dropColumn('user_id');
   })
 };
